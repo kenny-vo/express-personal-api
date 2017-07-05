722d9919aad60a30416449919c8a48123718e09f
Kenneth Vo <kennyvo91@gmail.com>
2017-07-04 21:37:25 -0500

    cleaned comments

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-// require express and other modules
 var express = require('express'),
     app = express();
 
-// parse incoming urlencoded form data
-// and populate the req.body object
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// allow cross origin requests (optional)
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -25,8 +21,7 @@ var db = require('./models');
  * ROUTES *
  **********/
 
-// Serve static files from the `/public` directory:
-// i.e. `/images`, `/scripts`, `/styles`
+
 app.use(express.static('public'));
 
 /*
@@ -143,7 +138,6 @@ app.delete('/api/vacations/:id', function (req, res) {
  * SERVER *
  **********/
 
-// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
 });
